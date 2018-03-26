e3e35465f5a24392d5026c20002ce128ee416730
Andre Branchizio <andrejbranch@gmail.com>
2018-03-26 11:11:06 -0600

    update base sample type and add BaseCryoblockEntity

--- Entity/Storage/BaseSampleType.php
+++ Entity/Storage/BaseSampleType.php
@@ -17,8 +17,28 @@ class BaseSampleType
      * @ORM\Column(name="name", type="string", length=255)
      * @JMS\Groups({"default"})
      * @Carbon\Searchable(name="name")
+     * @Gedmo\Versioned
      */
-    private $name;
+    protected $name;
+
+    /**
+     * @var \DateTime $deletedAt
+     *
+     * @ORM\Column(name="deleted_at", type="datetime", nullable=true)
+     * @JMS\Groups({"default"})
+     * @Gedmo\Versioned
+     */
+    protected $deletedAt;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Storage\Sample", mappedBy="sampleType")
+     */
+    protected $sampleTypeSamples;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Storage\DivisionSampleType", mappedBy="sampleType")
+     */
+    protected $divisionSampleTypes;
 
     /**
      * Set name
@@ -50,5 +70,29 @@ class BaseSampleType
     public function getStringLabel()
     {
         return $this->getName();
+    }
+
+    /**
+     * Gets the value of deletedAt.
+     *
+     * @return \DateTime $deletedAt
+     */
+    public function getDeletedAt()
+    {
+        return $this->deletedAt;
+    }
+
+    /**
+     * Sets the value of deletedAt.
+     *
+     * @param \DateTime $deletedAt $deletedAt the deleted at
+     *
+     * @return self
+     */
+    public function setDeletedAt($deletedAt)
+    {
+        $this->deletedAt = $deletedAt;
+
+        return $this;
     }
 }
