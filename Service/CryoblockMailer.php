643a5a39be52e04fc68b149f36bfc0cdb7a29811
Andre Branchizio <andrejbranch@gmail.com>
2018-03-30 14:11:02 -0600

    one method to rule them all

--- Service/CryoblockMailer.php
+++ Service/CryoblockMailer.php
@@ -24,65 +24,41 @@ class CryoblockMailer
         $this->mailer = $container->get('mailer');
     }
 
-    //Takes an array of group names looks up contact information for users in the set of groups and sends emails to all of them.
-    public function sendToGroup($subject, $template, $toArray = array(), $params = array(), $from = null)
+    public function send($subject, $template, $to, $params = array(), $from = null, $groups = array())
     {
-        $em = $this->container->get('doctrine.orm.default_entity_manager');
-        $sendTo = array();
-        foreach ($toArray as $to){
+        $content = $this->getTemplatingEngine()->render($template, $params);
+
+        $fromArray = [];
+        if (!$from) {
+
+            $from = $this->getLoggedInUser();
+            $fromEmail = $from->getEmail();
+            $fromName = '=?UTF-8?B?' . base64_encode($from->getFullName() . ' ' . '(' . $this->getAppName() . ')') . '?=';
+            $fromArray[$fromEmail] = $fromName;
+
+        } else {
+            $fromArray = $from;
+        }
+
+        if (count($groups)) {
+
+            $em = $this->container->get('doctrine.orm.default_entity_manager');
 
             $groups = $em->getRepository("Carbon\ApiBundle\Entity\Group")->findBy(
-                array('name' => $to)
-            );
-            $group = $groups[0];
-
-            $users = $em->getRepository("Carbon\ApiBundle\Entity\UserGroup")->findBy(
-                array('groupId' => $group->getId())
+                array('name' => $groups)
             );
 
-            foreach ($users as $user){
-                $sendTo[$user->getUser()->getEmail()] = $user->getUser()->getStringLabel();
+            foreach ($groups as $group) {
+                foreach ($group->getGroupUsers() as $groupUser) {
+                    $user = $groupUser->getUser();
+                    $to[$user->getEmail()] = $user->getStringLabel();
+                }
             }
+
         }
 
-        $content = $this->getTemplatingEngine()->render($template, $params);
-
-        $fromArray = [];
-        if (!$from) {
-
-            $from = $this->getLoggedInUser();
-            $fromEmail = $from->getEmail();
-            $fromName = '=?UTF-8?B?' . base64_encode($from->getFullName() . ' ' . '(' . $this->getAppName() . ')') . '?=';
-            $fromArray[$fromEmail] = $fromName;
-
-        } else {
-            $fromArray = $from;
-        }
-
-        $message = \Swift_Message::newInstance()
-            ->setSubject($subject)
-            ->setFrom($fromArray)
-            ->setTo($sendTo)
-            ->setBody($content, 'text/html')
-        ;
-
-        $this->mailer->send($message);
-    }
-
-    public function send($subject, $template, $to, $params = array(), $from = null)
-    {
-        $content = $this->getTemplatingEngine()->render($template, $params);
-
-        $fromArray = [];
-        if (!$from) {
-
-            $from = $this->getLoggedInUser();
-            $fromEmail = $from->getEmail();
-            $fromName = '=?UTF-8?B?' . base64_encode($from->getFullName() . ' ' . '(' . $this->getAppName() . ')') . '?=';
-            $fromArray[$fromEmail] = $fromName;
-
-        } else {
-            $fromArray = $from;
+        if (count($to) == 0) {
+            throw new \UnexpectedValueException('Can not send email to no one. $to array is empty');
         }
 
         $message = \Swift_Message::newInstance()
