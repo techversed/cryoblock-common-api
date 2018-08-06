19f24cbb3bd8bcc0eea2f4bf7791fb0921ffe6a9
Taylor Jones <tjonesster@gmail.com>
2018-08-06 16:48:14 -0500

    fixed the issue that we were having where the input template could not be downloaded. it was due to project samples not being in the default serializer group

--- Entity/Storage/BaseSample.php
+++ Entity/Storage/BaseSample.php
@@ -276,6 +276,7 @@ class BaseSample
 
     /**
     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Project\ProjectSample", mappedBy="sample")
+    * @JMS\Groups({"default"})
     */
     protected $projectSamples;
 
