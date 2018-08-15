b9299247ff181833bf1aaf2d3edc2795e62cea32
Taylor Jones <tjonesster@gmail.com>
2018-08-15 10:01:48 -0500

    ready to start testing the catalogListener

--- Listener/Storage/CatalogListener.php
+++ Listener/Storage/CatalogListener.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Storage\Sample;
 use AppBundle\Entity\Storage\Catalog;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\UnitOfWork;
 use Symfony\Bridge\Monolog\Logger;
 
 class CatalogListener
@@ -17,27 +18,29 @@ class CatalogListener
         $catalogRepo = $em->getRepository('AppBundle\Entity\Storage\Catalog');
         $sampleRepo = $em->getRepository('AppBundle\Entity\Storage\Sample');
 
-        foreach ($uow->getScheduledEntityInsertions() as $keyEntity => $entity) {
+        foreach ($uow->getScheduledEntityUpdates() as $keyEntity => $entity) {
 
             if ($entity instanceof Catalog) {
 
-                $catalogs = $catalogRepo->findBy(array('name' => $entity->getName()));
+                $catalogs = $catalogRepo->findBy( array('name' => $entity->getName()) );
 
-                $minId = 53337;
+                // $minId = 53337;
                 $catIdList = array();
+                // $catIdList[] = 53334;
 
                 foreach ($catalogs as $catalog) {
 
-                    // $minId = $minId ? ($minId < $catalog->getId() ? $minId : $catalog->getId()) : $catalog->getId();
+                    $minId = $minId ? ($minId < $catalog->getId() ? $minId : $catalog->getId()) : $catalog->getId();
 
                     $catIdList[] = $catalog->getId();
 
                 }
 
-                $query = $em->createQuery('update storage.sample set catalogId = ' . (string) $minId .  ' where catalogId in ' . '(' .  implode(', ', $catIdList) . ')');
+                $query = $em->createQuery('UPDATE AppBundle\Entity\Storage\Sample s SET s.catalogId = ' . (string) $minId .  ' where s.catalogId in (' .  implode(', ', $catIdList) . ') ');
+                // $query = $em->createQuery('UPDATE AppBundle\Entity\Storage\Sample s SET s.catalogId = 53337 where s.catalogId in (53335)');
                 $numUpdated = $query->execute();
 
-                $em->flush();
+                // $em->flush();
 
             }
         }
