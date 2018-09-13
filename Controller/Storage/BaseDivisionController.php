105e45494242e76a5a688dbfa8654970326b48f5
Taylor Jones <tjonesster@gmail.com>
2018-09-13 15:18:58 -0500

    added support for exporting storage divisions to excel sheets

--- Controller/Storage/BaseDivisionController.php
+++ Controller/Storage/BaseDivisionController.php
@@ -3,6 +3,7 @@
 namespace Carbon\ApiBundle\Controller\Storage;
 
 use AppBundle\Entity\Storage\Division;
+use Carbon\ApiBundle\Serializer\Dot;
 use Carbon\ApiBundle\Controller\CarbonApiController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -290,6 +291,221 @@ class BaseDivisionController extends CarbonApiController
         $serialized = $this->getSerializationHelper()->serialize($availableCells);
 
         return $this->getJsonResponse($serialized);
+    }
+
+    /**
+     * Handles the HTTP POST request for moving a division
+     *
+     * @Route("/storage/division/{id}/excelDownload", name="excel_download")
+     * @Method("GET")
+     *
+     * @return Response
+     */
+    public function excelDownload($id){
+
+        //$request = $this->getRequest();
+        //$data = json_decode($request->getContent(), true);
+
+        $fileName = 'Request ' . (string) $id . ' Input Samples Template.xls';
+
+        $objPHPExcel = new \PHPExcel();
+
+        $prodRequest = $this->getEntityManager()->getRepository('AppBundle\Entity\Storage\Division')->find($id);
+        $prodRequestInputSamples = $prodRequest->getSamples();
+        $prodRequestInputSample = $prodRequestInputSamples[0];
+        // print_r($prodRequestInputSample);
+        // echo $prodRequestInputSample->getId();
+        // die();
+        $importer = $this->container->get('sample.importer');
+        $sampleTypeMapping = $importer->getMapping($prodRequestInputSample->getSampleType());
+        $sampleTypeMapping = array_merge(array(
+            'Id' => array(
+                'prop' => 'id',
+                'bindTo' => 'id',
+                'errorProp' => array('id'),
+            )
+        ), $sampleTypeMapping);
+
+        $currentSample = 0;
+
+        $aRange = range('A', 'Z');
+        $current = 0;
+        foreach ($sampleTypeMapping as $label => $column) {
+
+            $objPHPExcel->getActiveSheet()->getColumnDimension($aRange[$current])->setWidth(15);
+            $cell = $objPHPExcel->getActiveSheet()->getCell($aRange[$current] . '1');
+            $cell->setValue($label);
+            $style = $objPHPExcel->getActiveSheet()->getStyle($aRange[$current] . '1');
+            $style->getFont()->setBold(true);
+
+            $current++;
+        }
+
+        $currentSample = 1;
+        $protectedLabels = array(
+            'Id',
+            'Sample Type',
+            'Catalog',
+            'Lot',
+            'Division',
+            'Division Row',
+            'Division Column',
+        );
+
+        $storageContainers = $this->getEntityManager()->getRepository('AppBundle\\Entity\\Storage\\StorageContainer')->findAll();
+
+        $storageContainerNames = array();
+        foreach ($storageContainers as $storageContainer) {
+            $storageContainerNames[] = $storageContainer->getName();
+        }
+        $storageContainerNames = implode(', ', $storageContainerNames);
+        $concentrationUnits = implode(', ', array(
+            'mg/mL',
+            'ng/uL',
+            'Molar',
+        ));
+
+        $statuses = implode(', ', array(
+            'Available',
+            'Depleted',
+            'Destroyed',
+            'Shipped',
+            'Incoming',
+        ));
+
+        foreach ($prodRequestInputSamples as $prodRequestInputSample) {
+
+            $current = 0;
+
+            $serializedInputSample = json_decode($this->getSerializationHelper()->serialize($prodRequestInputSample, array('template')), true);
+
+            $data = new Dot($serializedInputSample);
+
+            foreach ($sampleTypeMapping as $label => $column) {
+
+                $num = $currentSample + 1;
+                $cell = $aRange[$current] . $num;
+
+                $style = $objPHPExcel->getActiveSheet()->getStyle($cell);
+
+                if (in_array($label, $protectedLabels)) {
+
+                    $style->applyFromArray(array(
+                        'fill' => array(
+                            'type' => \PHPExcel_Style_Fill::FILL_SOLID,
+                            'color' => array('rgb' => 'fce7c2')
+                        )
+                    ));
+
+                    $style
+                        ->getProtection()
+                        ->setLocked(
+                            \PHPExcel_Style_Protection::PROTECTION_PROTECTED
+                        )
+                    ;
+
+                } else {
+
+                    $objPHPExcel->getActiveSheet()
+                    ->getStyle($cell)
+                    ->getProtection()
+                    ->setLocked(
+                        \PHPExcel_Style_Protection::PROTECTION_UNPROTECTED
+                    );
+
+                }
+
+                if ($label == 'Storage Container') {
+
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"'.$storageContainerNames.'"');
+
+                }
+
+                if ($label == 'Concentration Units') {
+
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"' . $concentrationUnits . '"');
+
+                }
+
+                if ($label == 'Status') {
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"' . $statuses . '"');
+
+                }
+
+                if (array_key_exists('mtm', $column) && $column['mtm']) {
+                    $itemIds = array();
+                    foreach ($data->get($column['prop']) as $item) {
+                        $itemIds[] = $item[$column['bindTo']];
+                    }
+                    if (count($itemIds)) {
+                        $objPHPExcel->getActiveSheet()->getCell($cell)->setValue(implode(',', $itemIds));
+                    }
+                } else {
+                    $objPHPExcel->getActiveSheet()->getCell($cell)->setValue($data->get($column['bindTo']));
+                }
+
+                $current++;
+            }
+
+            $currentSample++;
+
+        }
+
+        $objPHPExcel->getActiveSheet()->getProtection()->setSheet(true);
+        $objPHPExcel->getActiveSheet()->getProtection()->setSort(true);
+        $objPHPExcel->getActiveSheet()->getProtection()->setInsertRows(true);
+        $objPHPExcel->getActiveSheet()->getProtection()->setFormatCells(true);
+
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $objPHPExcel->setActiveSheetIndex(0);
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/vnd.ms-excel');
+        $response->headers->set('Content-Disposition', 'attachment;filename=test.xlsx');
+
+        ob_start();
+        $objWriter->save('php://output');
+        $content = ob_get_contents();
+        ob_end_clean();
+        $response->setContent($content);
+
+        return $response;
+
     }
 
 }
