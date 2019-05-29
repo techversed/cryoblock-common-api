7591e974f223ab0c6bd368b59d09b27a1266021f
Taylor Jones <tjonesster@gmail.com>
2019-05-29 11:58:38 -0500

    adding to the gridform controller to also support the lookup of the objects that are being created/updated

--- Controller/GridFormController.php
+++ Controller/GridFormController.php
@@ -225,6 +225,84 @@ class GridFormController extends CarbonApiController
     // The flow of this complete action is going to have all of the main permissions updated
 
 
+    // Might also give the option to look up other fields in the event that an id is not given
+    /*
+        This funciton is designed to take an object provided on the frontend, locate the
+        $em should be an entity manager
+        $object should be whatever you are looking to add to the frontend
+    */
+    protected function lookupObject($em, $obj)
+    {
+
+        // Check if there is an entity detail id in the array.
+        // If so look it up -- if not specified throw an exception
+            // If lookup failes throw an exception
+        if(!array_key_exists('entityDetailId', $obj))
+        {
+
+            throw new \Exception("there is no entity detail id provided in the object that you are looking up");
+
+        }
+
+        $entDet = $em->getRepository('Carbon\ApiBundle\Entity\EntityDetail')->find($obj['entityDetailId']);
+
+        if (!$entDet) {
+
+            throw new \Exception("There is no entity detail entry with the specified id number");
+
+        }
+
+        // Use the metadata stored on entity detail id in order to look up the object--
+            // If id is not given throw an exception
+            // if the object is not found then throw an exception
+
+        $objRepo = $em->getRepository($entDet->getObjectClassName());
+
+        if (array_key_exists('id', $obj)){
+
+            $foo =  $objRepo->find($obj['id']);
+            // $foo =  $objRepo->find(-1);
+
+            // If the object lookup failed throw an exception
+            if (!$foo) {
+
+                throw new \Exception("Thre is no entity detail with the specified id");
+
+            }
+
+        }
+        else {
+
+            /*
+                LONG TERM WE WOULD LIKE FOR THIS TO CREATE OBJECTS WHICH DO NOT EXIST YET -- IN THE SHORT TERM WE ARE JUST GOING TO THROW AN EXCEPTION IN THIS CASE
+            */
+
+            // $obj = new $entDet->getObjectClassName();
+            // We should finish writing this so that the entity will be created here in the event that it does not already exists
+
+            // $em->persist ...
+
+            // For now we are just going to throw an exception reminding us of what we need to finish building
+
+            throw new \Exception("Need to finish buidling the lookupObject function in GridFormController.php in common");
+
+        }
+
+        // $lookedUpObject = array();
+
+        return $lookedUpObject;
+
+    }
+
+    /*
+        This function takes an object which has been received from the entity manager and returns an array that conatains just the id
+        This type of input is needed to hand things off to forms. The OTO transformer which is present in forms is set up to only need this information ...
+
+    */
+    protected function prepForOTO($obj)
+    {
+        return array('id' => $obj->getId());
+    }
 
 
     // We are not done building this yet.
@@ -239,6 +317,30 @@ class GridFormController extends CarbonApiController
         $request = $this->getRequest();
         $data = json_decode($request->getContent(), true);
 
+        return $this->lookupObject($em, array("entityDetailId" => "2", 'id' => '1'));
+
+        // THIS IS ONLY INTENDED TO BE USED WITH THE TEST CASES -- IT SHOULD NOT MAKE IT INTO PRODUCTION
+        // This is obviously not final -- this is just for testing purposes
+        $storageContainer = $em->getRepository('AppBundle\Entity\Storage\StorageContainer')->findBy(array('name' => "Vial"))[0];
+        $storageContainer = array('id' =>$storageContainer->getId());
+
+        $sampleType = $em->getRepository('AppBundle\Entity\Storage\SampleType')->findBy(array('name' => "Primary Cells"))[0];
+        $sampleType = array('id' =>$sampleType->getId());
+
+
+
+
+        /*
+            THIS PORTION IS GOING TO
+        */
+
+
+
+
+
+        /*
+            END OF THE PARSING PORTION
+        */
 
         // for this first version we are going to cook up some sample data and use that instead of passing it with the request
 
@@ -249,28 +351,34 @@ class GridFormController extends CarbonApiController
             // It is also going to have a series of gridforms which are going to allow for additional metadata properties to be stored on the linker table entries for grid forms.
 
         // The metdata on linkertable use case
+        // This should have id added to it later on.
         $exampleData1 = array(
-            'updateType' => 'mtmParent',
+            'actionType' => 'mtmParent',
             'Entities' => array(
                 array(
                     'BaseMetadata' => array(),
                     'GridForms' => array(
-                        'association1' => array(
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
-                        ),
-                        'association2' => array(
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
-                        );
-                        'association3' => array(
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                            array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
-                        );
+                        array(
+                            'columns' => array(),
+                            'entries' => array(
+                                'association1' => array(
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
+                                ),
+                                'association2' => array(
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
+                                ),
+                                'association3' => array(
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                                    array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
+                                )
+                            )
+                        )
                     )
                 )
             )
@@ -278,31 +386,47 @@ class GridFormController extends CarbonApiController
 
         // The bulk update usecase
         // worth noting that the next level under grid forms is not really needed -- it is probably not necessary for us to separate out create, update ... etc but it would be a good idea for delete to be separate.
+
+        // echo $storageContainer['id'];
+
+        // print_r($storageContainer);
+        // die();
         $exampleData2 = array(
-            'updateType' => 'bulkEntity',
-            'Entitites' => array(
+            'actionType' => 'bulkEntity',
+            'Entities' => array(
                 'GridForms' => array(
-                    'create' => array(
-                        array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                        array('field1' => 'value1','field2' => 'value1','field3' => 'value1'),
-                        array('field1' => 'value1','field2' => 'value1','field3' => 'value1')
-                    ),
-                    'update' => array(),
-                    'delete' => array()
+                    'columns' => array(),
+                    'entries' => array(
+                        array('id' => 1, 'status' => 'Depleted','description' => 'this is the description','storageContainer' => $storageContainer, 'sampleType' => $sampleType)
+                        // array('id' => 2, 'field1' => 'value1','field2' => 'value1','field3' => 'value1'),
+                        // array('id' => 3, 'field1' => 'value1','field2' => 'value1','field3' => 'value1')
+                    )
+                    //,
+                    // 'update' => array(
+
+                    // ),
+                    // 'delete' => array(
+
+                    // )
                 )
             )
         );
-
-
 
         // If both of the use cases above are accounted for then there should not be very many things that we are going to struggle to handle.
 
         // This array and this loop are only going to be used during the development stage of this pipeline.
         $examples = array($exampleData1, $exampleData2);
 
-        foreach($examples as $example) {
 
-            if ($data['updateType'] == 'mtmParent') {
+// In the next stage of testing we are going to move to running both testcases
+        // foreach($examples as $example) {
+
+        if(true) {
+
+            // choose variable name other than example later on.
+            $example = $examples[1]; // right now we are just going to worry about getting the regular bulk import working.
+
+            if ($example['actionType'] == 'mtmParent') {
 
                 // asdfasdfasd
                 // Apply changes to the base entities
@@ -316,23 +440,80 @@ class GridFormController extends CarbonApiController
                 // Flush the entity manager
 
             }
+            elseif ($example['actionType'] == 'bulkEntity') { // This is essentially going to be the same as the sample importer -- it is likely that we can change the sample importer to use this class intead of doing things the way that they were previously being done.
 
-            elseif ($data['updateType'] == 'bulkEntity') {
+                // This is also going to need a series of transformers that are used when validating the submission
 
-                // Assumes that the gridform has 3 properties called create, update, delete
+                // This still needs to be populated
+                foreach ($example['Entities']['GridForms']['create'] as $element) {
 
-                // If the entity exists grab it,
-                // If it does not then create it. .
 
-                // Instead of handling this as was mentioned above it would probably be perfectly fine to just use form submission instead of wrigint all sorts of custom code to handle this....
-                // Sample importer should already handle things this way.
+                    // Assumes that the gridform has 3 properties called create, update, delete
 
-                // Update and create can be handled in the same portion of this -- this would require us to have field names that line up with the ones that are used in the formtype.
+                    // If the entity exists grab it,
+                    // If it does not then create it. .
 
+                    // Instead of handling this as was mentioned above it would probably be perfectly fine to just use form submission instead of wrigint all sorts of custom code to handle this....
+                    // Sample importer should already handle things this way.
+
+                    // Update and create can be handled in the same portion of this -- this would require us to have field names that line up with the ones that are used in the formtype.
+
+                    // testing
+
+                    // Set the classname -- this may be gotten from the entity detail or directly from the reqeust -- don't know how we want to handle this yet.
+                    // There should probably be an array that stores all of the classnames...
+
+                    $classname = 'AppBundle\Entity\Storage\Sample'; // This can be set in a number of different ways...
+
+                    // at this point in time this has been set to evalute to false all of the time... change that before production ..
+
+                    if (array_key_exists('id', $element)) {
+
+                        // get the objectclassname from the entity detail entry thing
+                        // $entity = find ...
+
+                        $created = false;
+                        $entity = $em->getRepository($classname)->find($element['id']);
+
+                    }
+                    else {
+
+                        $created = true;
+                        $entity = new $classname();
+
+                        // entity = new ...
+
+                    }
+
+                    $form = $this->createForm('sample', $entity); // in the final version this should not be hardcoded...
+                    $form->submit($element);
+
+                    if(!$form->isValid()){
+
+                        // This function is present witin CarbonApiController
+
+                        // We should probably complete all of the actions and just make a list of the things that have errors instead of having the whole submisssion break in the event that one of them fails --
+                        return $this->getFormErrorResponse($form);
+
+                    }
+
+                    if (array_key_exists('id', $element)) {
+
+                        $em->persist($entity);
+
+                    }
+
+                }
+
+                $em->flush();
+
+            // end of bulkEntry code
 
             }
-
         }
+
+
+        die();
 
         // Need to quickly validate that it would be possible to do bulk updates of non-nested data using this method...
             // Lets start out by assuming that we are going to need to use this in order to perform bulk updates on samples... what needs to be added?
@@ -466,6 +647,7 @@ class GridFormController extends CarbonApiController
 LIST OF OTHER CHANGES THAT ARE GOING TO BE NEEDED IN ORDER FOR THIS TO ALL WORK OUT.
     Importers may need additioanl properties
     Everything that you would like to update with this is going to need to have its own importer created
+    Entity detail needs to have the name of the form that needs to be grabbed.
     Entity detail is going to need to have the importer stored for each type of element that is allowed to be imported with this mechanism
     May need to have multiple different functions for bulk import -- not sure at this time if it makes more sense for us to have separate importers for bulk and linker updates or if it makes sense to add new functions to the importer depending upon which action you are attempting to make
     Entity detail should be expanded to have update, create, delete permissions roles on it -- since we are going to try to make something generic that is going to work with all type of entities it is going to be absolutely essential for us to have a mechanism to set this for individual entity types instead of setting a standard in the controller.
