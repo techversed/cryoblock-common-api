55bcbd1144c2367980d5381e43aedf626d1de62e
Taylor Jones <tjonesster@gmail.com>
2019-09-30 18:03:54 -0500

    grid form controller changes

--- Controller/GridFormController.php
+++ Controller/GridFormController.php
@@ -78,6 +78,365 @@ LIST OF OTHER CHANGES THAT ARE GOING TO BE NEEDED IN ORDER FOR THIS TO ALL WORK 
 class GridFormController extends CarbonApiController
 {
 
+// New Version of this -- this is really just going to be a repackaged sample import controller.
+
+
+// This is going to return the entities in the form which is requested
+
+    // public function downloadOutputTemplateAction()
+    // {
+    //     $request = $this->getRequest();
+    //     $data = json_decode($request->getContent(), true);
+    //     $outputTemplateType = $data['outputTemplateType'];
+
+    //     if ($outputTemplateType === 'CSV') {
+    //         return $this->getCSVOutputTemplateResponse();
+    //     }
+
+    //     if ($outputTemplateType === 'EXCEL') {
+    //         return $this->getOutputExcelTemplateResponse();
+    //     }
+
+    //     if ($outputTemplateType === 'GRIDFORM') {
+    //         return $this->getOutputGridformTemplateResponse();
+    //     }
+
+    //     return $this->handleError();
+
+    // }
+
+
+    // Stuff that we are going to handle when creating a default form for all content which is going to be held in a gridform
+    /*
+
+    // $request = $this->getRequest();
+    // $data = json_decode($request->getContent(), true);
+    // $totalOutputSamples = $data['totalOutputSamples'];
+    // $outputSampleDefaults = $data['outputSampleDefaults'];
+
+    // if ($outputSampleDefaults == null ) {
+        // $outputSampleDefaults = [];
+    // }
+
+    if (!$this->isMultiDimArray($outputSampleDefaults)) {
+        $temp = array();
+        for ($i =0; $i < $totalOutputSamples; $i++) {
+            $temp[] = $outputSampleDefaults;
+        }
+        $outputSampleDefaults = $temp;
+    }
+
+        */
+
+    // Trash
+    /*
+
+if (array_key_exists('outputSampleType', $data)) {
+            $outputSampleTypeId = $data['outputSampleType']['id'];
+        } else {
+            $outputSampleTypeId = 1;
+        }
+
+    */
+
+// Need to work on stripping this down a little bit more to get it all working
+    private function getOutputExcelTemplateResponse($gridContents, $importer)
+    {
+        $totalOutputSamples = count($gridContents);
+        $isUpdate = array_key_exists('id', $data);
+
+        $filename = $isUpdate ? 'Request ' . $data['id'] . ' Output Samples Template.xls' : $fileName = 'Sample Import Template.xls';
+
+
+        $objPHPExcel = new \PHPExcel();
+
+        // $sampleType = $this->getEntityManager()->getRepository('AppBundle\\Entity\\Storage\\SampleType')->find($outputSampleTypeId);
+
+        // $importer = $this->container->get('sample.importer');
+        $sampleTypeMapping = $importer->getMapping(); //This does not need an argument... ? ? ?
+        $mapping = $importer->getSampleGridFormColumnMap()
+
+        $currentSample = 0;
+
+        $aRange = range('A', 'Z');
+        $current = 0;
+
+        foreach ($sampleTypeMapping as $label => &$column) {
+            $objPHPExcel->getActiveSheet()->getColumnDimension($aRange[$current])->setWidth(15);
+            $cell = $objPHPExcel->getActiveSheet()->getCell($aRange[$current] . '1');
+            $cell->setValue($label);
+            $style = $objPHPExcel->getActiveSheet()->getStyle($aRange[$current] . '1');
+            $style->getFont()->setBold(true);
+
+            $current++;
+        }
+        unset($column); //Dear god why?
+
+        $currentSample = 1;
+
+        $protectedLabels = $isUpdate ? getUpdateProtectedLabels() : array();
+
+        $storageContainers = $this->getEntityManager()->getRepository('AppBundle\\Entity\\Storage\\StorageContainer')->findAll();
+
+        $storageContainerNames = array();
+
+        foreach ($storageContainers as $storageContainer) {
+            $storageContainerNames[] = $storageContainer->getName();
+        }
+
+        // $storageContainerNames = implode(', ', $storageContainerNames);
+        // $concentrationUnits = implode(', ', array(
+        //     'mg/mL',
+        //     'ng/uL',
+        //     'Molar',
+        //     'cells/mL',
+        //     'cells/uL',
+        // ));
+
+        // $volumeUnits = implode(', ', array(
+        //     'mL',
+        //     'uL'
+        // ));
+
+        // $statuses = implode(', ', array(
+        //     'Available',
+        //     'Depleted',
+        //     'Destroyed',
+        //     'Shipped',
+        //     'Incoming',
+        // ));
+
+        $currentOutputSampleIndex = 0;
+        while ($currentOutputSampleIndex < $totalOutputSamples) {
+
+            $current = 0;
+            foreach ($sampleTypeMapping as $label => $column) {
+
+                $num = $currentSample + 1;
+                $cell = $aRange[$current] . $num;
+
+                $style = $objPHPExcel->getActiveSheet()->getStyle($cell);
+
+                if (in_array($label, $protectedLabels)) {
+
+                    $style->applyFromArray(array(
+                        'fill' => array(
+                            'type' => \PHPExcel_Style_Fill::FILL_SOLID,
+                            'color' => array('rgb' => 'fce7c2')
+                        )
+                    ));
+
+                    $style
+                        ->getProtection()
+                        ->setLocked(
+                            \PHPExcel_Style_Protection::PROTECTION_PROTECTED
+                        )
+                    ;
+
+                } else {
+
+                    $objPHPExcel->getActiveSheet()
+                    ->getStyle($cell)
+                    ->getProtection()
+                    ->setLocked(
+                        \PHPExcel_Style_Protection::PROTECTION_UNPROTECTED
+                    );
+
+                }
+
+                if ($label == 'Storage Container') {
+
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"'.$storageContainerNames.'"');
+
+                }
+
+                if ($label == 'Concentration Units') {
+
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"' . $concentrationUnits . '"');
+
+                }
+
+                if ($label == 'Volume Units') {
+
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"' . $volumeUnits . '"');
+
+                }
+
+                if ($label == 'Status') {
+                    $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                    $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                    $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                    $objValidation->setAllowBlank(false);
+                    $objValidation->setShowInputMessage(true);
+                    $objValidation->setShowErrorMessage(true);
+                    $objValidation->setShowDropDown(true);
+                    $objValidation->setErrorTitle('Input error');
+                    $objValidation->setError('Value is not in list.');
+                    $objValidation->setPromptTitle('Pick from list');
+                    $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                    $objValidation->setFormula1('"' . $statuses . '"');
+
+                }
+                if (array_key_exists($column['prop'], $outputSampleDefaults[$currentOutputSampleIndex])) {
+                    if (is_array($outputSampleDefaults[$currentOutputSampleIndex][$column['prop']])) {
+                        $objValidation = $objPHPExcel->getActiveSheet()->getCell($cell)->getDataValidation();
+                        $objValidation->setType( \PHPExcel_Cell_DataValidation::TYPE_LIST );
+                        $objValidation->setErrorStyle( \PHPExcel_Cell_DataValidation::STYLE_INFORMATION );
+                        $objValidation->setAllowBlank(false);
+                        $objValidation->setShowInputMessage(true);
+                        $objValidation->setShowErrorMessage(true);
+                        $objValidation->setShowDropDown(true);
+                        $objValidation->setErrorTitle('Input error');
+                        $objValidation->setError('Value is not in list.');
+                        $objValidation->setPromptTitle('Pick from list');
+                        $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                        $objValidation->setFormula1('"' . implode(', ', $outputSampleDefaults[$currentOutputSampleIndex][$column['prop']]) . '"');
+                        $objPHPExcel->getActiveSheet()->getCell($cell)->setValue($outputSampleDefaults[$currentOutputSampleIndex][$column['prop']][0]);
+                    } else {
+
+                        $objPHPExcel->getActiveSheet()->getCell($cell)->setValue($outputSampleDefaults[$currentOutputSampleIndex][$column['prop']]);
+
+                    }
+
+                }
+
+                $current++;
+            }
+
+            $currentSample++;
+            $currentOutputSampleIndex++;
+
+        }
+        if (array_key_exists('id', $data)) {
+            $objPHPExcel->getActiveSheet()->getProtection()->setSheet(true);
+            $objPHPExcel->getActiveSheet()->getProtection()->setSort(true);
+            $objPHPExcel->getActiveSheet()->getProtection()->setInsertRows(true);
+            $objPHPExcel->getActiveSheet()->getProtection()->setFormatCells(true);
+        }
+
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $objPHPExcel->setActiveSheetIndex(0);
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/vnd.ms-excel');
+        $response->headers->set('Content-Disposition', 'attachment;filename=test.xlsx');
+
+        ob_start();
+        $objWriter->save('php://output');
+        $content = ob_get_contents();
+        ob_end_clean();
+        $response->setContent($content);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/grid-form/download/{entDetId}", name="grid_form_download")
+     * @Method("POST")
+     *
+     * @return Response
+     */
+    public function downloadTemplateAction($entDetId)
+    {
+        $request = $this->getRequest();
+        $request = json_decode($request->getContent(), true);
+        $outputTemplateType = $data['outputTemplateType'];
+
+        $em = $this->getEntityManager();
+        $entDet = $em->getRepository('Carbon\ApiBundle\Entity\EntityDetail')->find($entDetId);
+
+        $importerClass = $entDet->getImporterClass();
+        $importer = $this->container->get($importerClass);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/grid-form/validate/{entDetId}", name="grid_form_validate")
+     * @Method("POST")
+     *
+     * @return Response
+     */
+    public function validateTemplate($entDetId)
+    {
+
+    }
+
+    protected function readExcel($requestContent, $importer)
+    {
+
+
+    }
+
+    protected function returnExcel()
+    {
+
+    }
+
+    protected function readCsv($requestContent, $importer)
+    {
+
+    }
+
+    protected function returnCSV()
+    {
+
+    }
+
+    protected function readGridForm($requestContent, $importer)
+    {
+
+    }
+
+    protected function returnGridForm()
+    {
+    }
+
+    protected function validateGridForm($form)
+    {
+
+    }
+
+
+
+// First attempt at doing this
+
+
     /*
 
         return new Response($data, $status, array(
@@ -96,7 +455,8 @@ class GridFormController extends CarbonApiController
      *
      * @return Response
      */
-    public function getFormSet() {
+    public function getFormSet()
+    {
 
 
         // Check permissions
